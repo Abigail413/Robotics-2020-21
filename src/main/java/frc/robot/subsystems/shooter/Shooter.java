cf83e449b505a52c8a2698aa54b426a08ac0a6c9
Abigail413 <abigail_burrow@isd31.net>
2021-03-29 15:25:27 -0500

    add if statement to choose between RPMs

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -135,33 +135,45 @@ public void collect(double intakeVolts) {
 
   }
   /**
-   * This calculates the rotations per minute necessary to shoot a ball into the target from a given distance. It is the positive half of a quadratic formula
+   * This calculates the rotations per minute necessary to shoot a ball into the target from a given distance. It chooses between two calculated RPMs and chooses the lesser positive one.
    * @param distanceMeters from target
    * @return RPM appropriate for the distance
    */
-  private double calculateRPMPos(double distanceMeters) {
+  private double calculateRPM(double distanceMeters) {
     double bValue = ((kTargetToCameraHeight * Math.cos(kShooterAngle)) / Math.pow(distanceMeters, 2)); 
     double aValue = -((Math.sin(kShooterAngle)) / distanceMeters);
 
-    double shooterVelocity = (-(bValue) + Math.sqrt(Math.pow(bValue, 2) - (4 * aValue * kAccelerationGravity))) / (2 * Math.sin(aValue));
-    double radPerSecond = shooterVelocity / kShooterWheelRadiusMeters;
+    double shooterVelocityPos = (-(bValue) + Math.sqrt(Math.pow(bValue, 2) - (4 * aValue * kAccelerationGravity))) / (2 * Math.sin(aValue));
+    double shooterVelocityNeg = (-(bValue) - Math.sqrt(Math.pow(bValue, 2) - (4 * aValue * kAccelerationGravity))) / (2 * Math.sin(aValue));
 
-    return Units.radiansPerSecondToRotationsPerMinute(radPerSecond);
+    double radPerSecondPos = shooterVelocityPos / kShooterWheelRadiusMeters;
+    double radPerSecondNeg = shooterVelocityNeg / kShooterWheelRadiusMeters;
+
+    double RPMPos = Units.radiansPerSecondToRotationsPerMinute(radPerSecondPos);
+    double RPMNeg = Units.radiansPerSecondToRotationsPerMinute(radPerSecondNeg);
+
+    if (RPMPos < RPMNeg && RPMPos > 0) { // If the RPM + is less than - and positive, use it for a lower arc
+      return RPMPos;
+
+    } else if (RPMNeg < RPMPos && RPMNeg > 0) { // If the RPM - is less than + and positive, use it for a lower arc
+      return RPMNeg;
+
+    } else if (RPMPos == RPMNeg && RPMNeg > 0) { // If they are the same value (and positive), return that value
+      return RPMNeg;
+
+    } else if (RPMNeg > 0) { // Return RPMNeg if positive, this is here in case RPMPos is negative
+      return RPMNeg;
+
+    } else if (RPMPos > 0) { // Return RPMPos if positive, this is here in case RPMNeg is negative
+      return RPMPos;
+
+    } else {
+      return 0; // something went wrong, let us not break things. It could be that RPMNeg and RPMPos are both negative
+    }
+
   }
- /**
-   * This calculates the rotations per minute necessary to shoot a ball into the target from a given distance. It is the negative half of a quadratic formula
-   * @param distanceMeters from target
-   * @return RPM appropriate for the distance
-   */
-  private double calculateRPMNeg(double distanceMeters) {
-    double bValue = ((kTargetToCameraHeight * Math.cos(kShooterAngle)) / Math.pow(distanceMeters, 2)); 
-    double aValue = -((Math.sin(kShooterAngle)) / distanceMeters);
+  
 
-    double shooterVelocity = (-(bValue) - Math.sqrt(Math.pow(bValue, 2) - (4 * aValue * kAccelerationGravity))) / (2 * Math.sin(aValue));
-    double radPerSecond = shooterVelocity / kShooterWheelRadiusMeters;
-
-    return Units.radiansPerSecondToRotationsPerMinute(radPerSecond);
-  }
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
